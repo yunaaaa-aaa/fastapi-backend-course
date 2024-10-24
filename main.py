f543c5d01de93022606114a4ca7486f7cc58b469
yunaaaa-aaa <yuna094516@gmail.com>
2024-10-24 16:04:32 +0800

    Modify main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name:str
+    price:float
+    description:str=None
 
 @app.get("/") #定義路徑 get->方法
 def read_root(): #當使用者在瀏覽器打開這個路徑，這個function就會回傳
@@ -9,3 +14,8 @@ def read_root(): #當使用者在瀏覽器打開這個路徑，這個function就
 @app.get('/哈囉')
 def read_hello():
     return '<p>Hello World</p>'
+
+@app.post("/items")
+def create_item(item:Item):
+    print(f"Received item: {item}")
+    return {"message":"Item received","item":item}
