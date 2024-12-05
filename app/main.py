ceb9cfd269864435425cbaaf090aebd3d8c2e228
yunaaaa-aaa <yuna094516@gmail.com>
2024-12-05 15:21:20 +0800

    Alter title to limit 100 chars and add due_date field

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
@@ -20,9 +20,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 class Todo(Base):
     __tablename__="todos"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    title = Column(String(100), nullable=False)
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
+    due_date = Column(Date, nullable=True)
 
 #Initialize DataBase's Table
 Base.metadata.create_all(bind=engine)
